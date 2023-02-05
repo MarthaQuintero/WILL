f156f5695241b1fd59b36fb5e1287a0b7addcc04
MarthaQuintero <lic.marthaquintero94@gmail.com>
2023-02-05 11:22:40 -0500

    Ejercicio 2

--- 02.js
+++ 02.js
@@ -4,7 +4,7 @@
   código dentro de las funciones ya definidas. 
   No comentar la funcion 
 */
-function stringMasLarga(strings) {
+//function stringMasLarga(strings) {
   // La función llamada 'stringMasLarga', recibe como argumento un arreglo de strings llamado 'strings'
   // y debe devolver el string más largo que hay en el arreglo (Es decir el de mayor cantidad de caracteres)
   // Ej:
@@ -12,7 +12,12 @@ function stringMasLarga(strings) {
   // stringMasLarga(['JavaScript', 'HTML', 'CSS']); debe retornar 'JavaScript'
 
   // Tu código aca
+  function stringMasLarga(strings) {
+    return strings.reduce((acc, val) => {
+        return (acc.length > val.length) ? acc : val;
+    }, '');
 }
+console.log(stringMasLarga(["Hola", "Soy", "Martha"]));
 
 // No modifiques nada debajo de esta linea //
 
